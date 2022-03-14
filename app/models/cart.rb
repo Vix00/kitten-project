3e70b0f57a0a268957933523768f016ed7d72fa4
Victoria <victoria.fely@hotmail.fr>
2022-03-14 19:35:30 +0100

    heyhey

---
+++ app/models/cart.rb
@@ -0,0 +1,8 @@
+#Penser à mettre des comments globals 
+class Cart < ApplicationRecord
+	has_one :user
+	has_many :cart_items, dependent: :destroy
+	has_many :items, through: :cart_items
+
+	validates :user, presence: true, uniqueness: true
+end
---
+++ app/models/order_item.rb
@@ -0,0 +1,8 @@
+class OrderItem < ApplicationRecord
+	belongs_to :order
+	belongs_to :item
+
+	validates :order, presence: true, uniqueness: true
+	validates :item, presence: true, uniqueness: true
+
+end
